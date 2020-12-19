b1ba03d68dd272eee6e420dbd6bd02fb1350e045
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2020-12-19 15:32:27 +0900

    Update DRLEnv.py

--- DRLEnv.py
+++ DRLEnv.py
@@ -57,7 +57,7 @@ class FedEnv(gym.Env):
         for i in range (2):
             P_new = [None for m in range (self.client)]
             for x in range (self.client):
-                P_new,temp = self.task.Local_agg(self.Model[x],x,self.client,action,self.latency)
+                P_new[x],temp = self.task.Local_agg(self.Model[x],x,self.client,action,self.latency)
                 
                 Tim.append(temp)
         # update     

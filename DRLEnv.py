b525cc7fd3d5c0ab6b19d200f3bf8737d2b2ed18
chenfahao <chenfh612@163.com>
2020-12-19 14:54:26 +0900

    code

---
+++ DRLEnv.py
@@ -0,0 +1,117 @@
+import gym
+from CNN import cnn
+import random
+import math
+import networkx as nx
+import numpy as np
+
+class FedEnv(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 2
+    }
+    def __init__(self, Client, k ):
+        
+        self.client = Client
+        self.p = 0.5
+        self.Model = []
+
+        # small world
+        self.G = nx.watts_strogatz_graph(n = self.client, k = k, p = self.p)
+
+        # To DGL graph
+        # self.g = dgl.from_networkx(self.G)
+
+        # latency simulation
+        self.latency = [[0 for i in range (self.client)]for j in range (self.client)]
+        for i in range (self.client):
+            for j in range (self.client):
+                self.latency[i][j] = random.randint(1,20)
+
+        self.task = cnn(Client = self.client, Dataset = 'CIFAR10', Net = 'MobileNet')    # num of clients, num of neighbors, dataset, network
+        self.Model, self.global_model = self.task.Set_Environment(Client)
+
+    
+
+    def step(self, action, epoch):
+
+        # # GAT network
+        # net = GATLayer(self.g,in_dim = 864,out_dim = 20)
+
+        Tim, Loss = [], []
+        # Loss = [0 for i in range (Client)]
+
+        P = self.task.CNN_train(epoch, self.client)
+
+        for i in range (self.client):
+            self.Model[i].load_state_dict(P[i])
+
+        # global model   
+        self.global_model = self.task.Global_agg(self.client) 
+        
+        accuracy = self.task.CNN_test(epoch,self.Model[0])
+
+        # aggregate local model
+        # Step 1: calculate the weight for each neighborhood
+        # Step 2: aggregate the model from neighborhood
+        for i in range (2):
+            P_new = [None for m in range (self.client)]
+            for x in range (self.client):
+                P_new[x], temp = self.task.Local_agg(self.Model[x],x,self.client,action,self.latency)
+                Tim.append(temp)
+        # update     
+        for client in range (self.client):
+            self.Model[client].load_state_dict(P_new[client])
+
+        t = self.task.step_time(Tim)
+
+
+        # PCA
+        parm_local = {}
+        Name = []
+        S_local = [None for i in range (self.client)]
+        for i in range (self.client):
+            S_local[i] = []
+            for name, parameters in self.Model[i].named_parameters():
+                # print(name,':',parameters.size())
+                parm_local[name]=parameters.detach().cpu().numpy()
+                Name.append(name)
+            
+            for a in parm_local[Name[0]][0::].flatten():
+                    aa = a
+                    S_local[i].append(aa)
+        s = np.array(S_local).flatten()
+        # self.toCsv(times,score)
+        reward = pow(512, accuracy-0.98)
+
+        return t, accuracy, s, reward
+
+
+
+    
+    def reset(self):
+        self.Model, global_model = self.task.Set_Environment(self.client)
+        # PCA
+        parm_local = {}
+        Name = []
+        S_local = [None for i in range (self.client)]
+        for i in range (self.client):
+            S_local[i] = []
+            for name, parameters in self.Model[i].named_parameters():
+                # print(name,':',parameters.size())
+                parm_local[name] = parameters.detach().cpu().numpy()
+                Name.append(name)
+            
+            for a in parm_local[Name[0]][0::].flatten():
+                    aa = a
+                    S_local[i].append(aa)
+        s = np.array(S_local).flatten()
+        # print (s.size)
+
+        return s
+
+    def render(self, mode='human'):
+        return None
+        
+    def close(self):
+        return None
---
+++ run.py
@@ -0,0 +1,34 @@
+from DRLEnv import FedEnv
+from DDPG import Agent
+import torch
+import numpy as np
+from collections import deque
+
+if __name__ == '__main__':
+    print (torch.cuda.is_available())
+    epoches, print_every = 200, 100
+    env = FedEnv(Client = 10, k = 2)  # env
+    agent = Agent(state_size=8640, action_size=100, random_seed=2)  # agent
+    scores_deque = deque(maxlen=print_every)
+    scores = []
+    for i_episode in range(1, 200+1):
+        X, Y = [], []  # x and y axis
+        state = env.reset()
+        agent.reset()
+        score = 0
+        for t in range(epoches):
+            action = agent.act(state)
+            time, accuracy, next_state, reward = env.step(action,t)
+            agent.step(state, action, reward, next_state)
+            state = next_state
+            score += reward
+        scores_deque.append(score)
+        scores.append(score)
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)), end="")
+        torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+        torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+        if i_episode % print_every == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+
+
+

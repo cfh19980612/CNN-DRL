236d39480c74b4b4806b7f77093f44057a1813c5
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2020-12-22 20:25:56 +0900

    Update run.py

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
             if accuracy > 0.8:
                 env.save_acc(X,Y)
                 break
-            pbar.set_description("Epoch: {}".format(i), "Accuracy: {}".format(accuracy))
+            pbar.set_description("Epoch: {} Accuracy: {}".format(i，accuracy))
         scores_deque.append(score)
         scores.append(score)
         print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)), end="")

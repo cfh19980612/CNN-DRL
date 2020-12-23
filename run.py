96aec22d2cee8f4acfb1ab36b0dad5c9cf39e701
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2020-12-23 10:54:46 +0900

    Update run.py

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         state = env.reset()
         agent.reset()
         score = 0
-        pbar = tqdm(range(2))
+        pbar = tqdm(range(100))
         for i in pbar:
             action = agent.act(state)
             time, accuracy, next_state, reward = env.step(action,i)

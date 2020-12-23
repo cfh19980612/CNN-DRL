37da570d442136ead03cd0ecff1b456ac401cd27
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2020-12-23 10:43:16 +0900

    Update run.py

--- run.py
+++ run.py
@@ -13,13 +13,14 @@ if __name__ == '__main__':
     agent = Agent(state_size=5184, action_size=9, random_seed=2)  # agent
     scores_deque = deque(maxlen=print_every)
     scores = []
+    X_reward = []
     for i_episode in range(1, 200+1):
         X, Y = [], []  # x and y axis for test_data
         start_time = 0
         state = env.reset()
         agent.reset()
         score = 0
-        pbar = tqdm(range(100))
+        pbar = tqdm(range(2))
         for i in pbar:
             action = agent.act(state)
             time, accuracy, next_state, reward = env.step(action,i)
@@ -34,20 +35,21 @@ if __name__ == '__main__':
             score += reward
             
             # end?
-            if accuracy > 0.82:
+            if accuracy > 0.825:
                 env.save_acc(X,Y)
                 break
             pbar.set_description("Epoch: %d Accuracy: %.3f" %(i, accuracy))
         scores_deque.append(score)
         scores.append(score)
-        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)), end="")
+        X_reward.append(i_episode)
+        print('\rEpisode {}\tAverage Score: {:.3f}'.format(i_episode, np.mean(scores_deque)), end="")
         torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
         torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
         
         # save reward
-        dataframe = pd.DataFrame(i_episode, columns=['X'])
-        dataframe = pd.concat([dataframe, pd.DataFrame(np.mean(scores_deque),columns=['Y'])],axis=1)
-        dataframe.to_csv("/home/ICDCS/Reward_data/reward.csv",mode='a',header = False,index=False,sep=',')
+        dataframe = pd.DataFrame(X_reward, columns=['X'])
+        dataframe = pd.concat([dataframe, scores,columns=['Y'])],axis=1)
+        dataframe.to_csv("/home/ICDCS/Reward_data/reward.csv",mode='w',header = False,index=False,sep=',')
         
         if i_episode % print_every == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))

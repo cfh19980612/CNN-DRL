a78a87a3d12903c6b8e368ac138ece6bd36c2d99
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2020-12-28 16:07:38 +0900

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -187,12 +187,12 @@ class cnn(nn.Module):
         for key, value in P.items():
             m = 0
             for j in range (Client):
-                    
-                # P[key] = P[key] + (self.g.edata['a'][self.g.edge_ids(i,j)])[0,]*Q[j][key]
-                P[key] = P[key] + p[i,j]*Q[j][key]
-                # P[key] = torch.true_divide(P[key],2)
-                m = m + p[i,j]
-            P[key] = torch.true_divide(P[key],m)
+                if p[i,j] > 0:
+                    # P[key] = P[key] + (self.g.edata['a'][self.g.edge_ids(i,j)])[0,]*Q[j][key]
+                    P[key] = P[key] + Q[j][key]
+                    # P[key] = torch.true_divide(P[key],2)
+                    m = m + 1
+            P[key] = torch.true_divide(P[key],m+1)
             
         for j in range (Client):
             # if self.G.has_edge(i,j):

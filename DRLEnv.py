5d6b7872345529d73185df4c69b89025395d0274
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2020-12-30 14:46:58 +0900

    Update DRLEnv.py

--- DRLEnv.py
+++ DRLEnv.py
@@ -84,7 +84,7 @@ class FedEnv(gym.Env):
         s = np.array(S_local).flatten()
         
         # self.toCsv(times,score)
-        reward = pow(128, accuracy-0.8)-0.01*t
+        reward = pow(64, accuracy-0.8)-1
 
         return t, accuracy, s, reward
 

fa2e7edc69cc0f4e39c783173f2dd9d5f7978272
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2021-01-03 21:12:57 +0900

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -239,10 +239,13 @@ class cnn(nn.Module):
         for key, value in P.items():
             m = 0
             for j in range (Client):
-                if i == j:
-                    P[key] = P[key]*(Imp[i,j]/Imp[i].sum())
-                else:
-                    P[key] = P[key] + (Imp[i,j]/Imp[i].sum())*Q[j][key]
+                if i =! j:
+                    if Imp[i,j] > 0:
+#                     P[key] = P[key] + (Imp[i,j]/Imp[i].sum())*Q[j][key]
+                        P[key] = P[key] + Q[j][key]
+                        m += 1
+            P[key] = torch.true_divide(P[key]/m+1)
+    
             
         for j in range (Client):
             # if self.G.has_edge(i,j):

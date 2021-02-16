ba388fb6a5063ba87cc6045354f68f63b15a0cd0
Fahao Chen <chenfh612@gmail.com>
2021-02-16 16:38:27 +0900

    .

--- CNN.py
+++ CNN.py
@@ -281,8 +281,8 @@ class cnn(nn.Module):
 #                     P[key] = P[key] + (Imp[i,j]/Imp[i].sum())*Q[j][key]
                         P[key] = P[key] + Q[j][key]
                         m += 1
-            # P[key] = torch.true_divide(P[key],m+1)
-            P[key] = P[key]/m+1
+            P[key] = torch.true_divide(P[key],m+1)
+            # P[key] = P[key]/m+1
 
         for j in range (Client):
             # if self.G.has_edge(i,j):

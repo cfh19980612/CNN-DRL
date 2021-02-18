a2aa9e4dccc493afc5b4e55e9b324a17aece1160
Fahao Chen <chenfh612@gmail.com>
2021-02-19 01:46:09 +0900

    .

--- FedAVG.py
+++ FedAVG.py
@@ -26,7 +26,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def Set_dataset(dataset):
     if dataset == 'CIFAR10':
         parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-        parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+        parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
         parser.add_argument('--resume', '-r', action='store_true',
                             help='resume from checkpoint')
         parser.add_argument('--epoch',default=100,type=int,help='epoch')

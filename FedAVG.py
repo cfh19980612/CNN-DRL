91368b00250e1e2fb98aadf53031400c0eacf940
Fahao Chen <chenfh612@gmail.com>
2021-02-19 01:34:00 +0900

    3

--- FedAVG.py
+++ FedAVG.py
@@ -213,7 +213,7 @@ def run(dataset, net, client):
             model[j].load_state_dict(Temp[j])
         global_temp = MobileNet()
         global_model.load_state_dict(Aggregate(copy.deepcopy(model), client))
-        acc, loss = Test(global_model, testloader)
+        acc, loss = Test(global_temp, testloader)
         pbar.set_description("Epoch: %d Accuracy: %.3f Loss: %.3f Time: %.3f" %(i, acc, loss, start_time))
         # for j in range (client):
         #     model[j].load_state_dict(global_temp.state_dict())

f3617be600330a44f3d183d0a9081b3a4ca1c790
Fahao Chen <chenfh612@gmail.com>
2021-02-18 23:00:33 +0900

    .

--- FedAVG.py
+++ FedAVG.py
@@ -211,7 +211,7 @@ def run(dataset, net, client):
         Temp, process_time = Train(model, optimizer, client, trainloader)
         for j in range (client):
             model[j].load_state_dict(Temp[j])
-        global_model.load_state_dict(Aggregate(model, client))
+        global_model.load_state_dict(Aggregate(copy.deepcopy(model), client))
         acc, loss = Test(global_model, testloader)
         # for j in range (client):
         #     model[j].load_state_dict(global_temp.state_dict())

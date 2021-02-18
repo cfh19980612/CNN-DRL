1c7ed6f2b78d480bcb7b283a126d83f17d300768
Fahao Chen <chenfh612@gmail.com>
2021-02-18 18:39:19 +0900

    .

--- FedAVG.py
+++ FedAVG.py
@@ -185,9 +185,9 @@ def Aggregate(model, client):
 
     P = copy.deepcopy(model[0].state_dict())
     for key in P.keys():
-        for i in range (9):
+        for i in range (2):
             P[key] += P[key]
-        P[key] = torch.true_divide(P[key],10)
+        P[key] = torch.true_divide(P[key],4)
 
     return P
 

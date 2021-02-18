eb55083fd77956ddd470f7fa07fb90c9723f7e5f
Fahao Chen <chenfh612@gmail.com>
2021-02-18 21:34:46 +0900

    .

--- FedAVG.py
+++ FedAVG.py
@@ -204,7 +204,7 @@ def run(dataset, net, client):
             model[j].load_state_dict(Temp[j])
         global_model.load_state_dict(Aggregate(model, client))
         acc, loss = Test(global_model, testloader)
-        for i in range (client):
+        for j in range (client):
             model[j].load_state_dict(global_model.state_dict())
         start_time += process_time
         pbar.set_description("Epoch: %d Accuracy: %.3f Loss: %.3f Time: %.3f" %(i, acc, loss, start_time))

77f39ee4ef3349a6c8f3c2c00227b89154b795cf
Fahao Chen <chenfh612@gmail.com>
2021-02-19 22:37:50 +0900

    .

--- plot.py
+++ plot.py
@@ -2,25 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-File = open('/home/cifar-gcn-drl/Test_data/FedAVG.csv')  # 打开csv文件
-Reader = csv.reader(File)  # 读取csv文件
-Data = list(Reader)  # csv数据转换为列表
-length_vol = len(Data)  # 得到数据行数
-length_clu = len(Data[0])  # 得到每行长度
+x1, y2 = np.loadtxt('/home/cifar-gcn-drl/Test_data/FedAVG_ACC.csv',delimiter=',',unpack = True)
 
-time = list()
-acc = list()
-loss = list()
 
-for i in range(length_vol):
-    time.append(Data[i][0])
-    acc.append(Data[i][1])
-    loss.append(Data[i][2])
-
-plt.plot(time, acc, [0.1,0.53,0.93], label = 'FedAVG')
+plt.plot(x1,y1, [0.1,0.53,0.93], label = 'FedAVG')
 plt.xlabel('Time')
 plt.ylabel('Accuracy')
 plt.title('Comparasion')
 plt.legend()
 
-plt.savefig("/home/cifar-gcn-drl/Fig/Accuracy.png")
+plt.savefig("/home/cifar-gcn-drl/Fig/Accuracy.eps")

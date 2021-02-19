09acd2e33be51bf8f2a40f5a2fc4fcaccef189f9
Fahao Chen <chenfh612@gmail.com>
2021-02-19 22:38:19 +0900

    .

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-x1, y2 = np.loadtxt('/home/cifar-gcn-drl/Test_data/FedAVG_ACC.csv',delimiter=',',unpack = True)
+x1, y1 = np.loadtxt('/home/cifar-gcn-drl/Test_data/FedAVG_ACC.csv',delimiter=',',unpack = True)
 
 
 plt.plot(x1,y1, [0.1,0.53,0.93], label = 'FedAVG')

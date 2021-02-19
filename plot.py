a677160cd1de114f44bcefc2f5d9c400d313009e
Fahao Chen <chenfh612@gmail.com>
2021-02-19 16:54:05 +0900

    .

--- plot.py
+++ plot.py
@@ -5,8 +5,8 @@ import csv
 File = open('/home/cifar-gcn-drl/Test_data/FedAVG.csv')  # 打开csv文件
 Reader = csv.reader(File)  # 读取csv文件
 Data = list(Reader)  # csv数据转换为列表
-length_vol = len(exampleData)  # 得到数据行数
-length_clu = len(exampleData[0])  # 得到每行长度
+length_vol = len(Data)  # 得到数据行数
+length_clu = len(Data[0])  # 得到每行长度
 
 time = list()
 acc = list()

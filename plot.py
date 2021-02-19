d1bf58f25e138a36cfba098f8b8019c8082a2cbb
Fahao Chen <chenfh612@gmail.com>
2021-02-19 22:56:02 +0900

    .

--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
+# accuracy
 x1, y1 = np.loadtxt('/home/cifar-gcn-drl/Test_data/FedAVG_ACC.csv',delimiter=',',unpack = True)
 
 
@@ -12,3 +13,17 @@ plt.title('Comparasion')
 plt.legend()
 
 plt.savefig("/home/cifar-gcn-drl/Fig/Accuracy.eps")
+
+
+
+# loss
+x2, y2 = np.loadtxt('/home/cifar-gcn-drl/Test_data/FedAVG_LOSS.csv',delimiter=',',unpack = True)
+
+
+plt.plot(x1,y1, [0.75,0.24,1], label = 'FedAVG')
+plt.xlabel('Time')
+plt.ylabel('Loss')
+plt.title('Comparasion')
+plt.legend()
+
+plt.savefig("/home/cifar-gcn-drl/Fig/Loss.eps")

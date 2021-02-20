6a65bec4209f544f2ad8901f70f71d832b5fe314
Fahao Chen <chenfh612@gmail.com>
2021-02-20 15:57:55 +0900

    .

--- DRLEnv.py
+++ DRLEnv.py
@@ -45,7 +45,7 @@ class FedEnv(gym.Env):
     def Set_dataset(self):
         if self.dataset == 'CIFAR10':
             parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-            parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+            parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
             parser.add_argument('--resume', '-r', action='store_true',
                                 help='resume from checkpoint')
             args = parser.parse_args()
@@ -69,7 +69,7 @@ class FedEnv(gym.Env):
             trainset = torchvision.datasets.CIFAR10(
                 root='/home/ICDCS/cifar-10-batches-py/', train=True, download=True, transform=transform_train)
             trainloader = torch.utils.data.DataLoader(
-                trainset, batch_size=256, shuffle=True, num_workers=2)
+                trainset, batch_size=128, shuffle=True, num_workers=2)
 
             testset = torchvision.datasets.CIFAR10(
                 root='/home/ICDCS/cifar-10-batches-py/', train=False, download=True, transform=transform_test)
@@ -156,7 +156,7 @@ class FedEnv(gym.Env):
         # aggregate local model
         # Step 1: calculate the weight for each neighborhood
         # Step 2: aggregate the model from neighborhood
-        for i in range (3):
+        for i in range (2):
             P_new = [None for m in range (self.client)]
             for x in range (self.client):
                 P_new[x],temp = self.task.Local_agg(self.Model, x, self.client,action, self.latency)

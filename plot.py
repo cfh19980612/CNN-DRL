b42d4a150f9d4d427c99ff152dc9b29b984b78c2
Fahao Chen <chenfh612@gmail.com>
2021-02-20 20:04:26 +0900

    .

--- plot.py
+++ plot.py
@@ -3,9 +3,9 @@ import numpy as np
 
 # accuracy
 x1, y1 = np.loadtxt('/home/cifar-gcn-drl/Test_data/FedAVG_ACC.csv',delimiter=',',unpack = True)
-plt.plot(x1,y1, 'g--', label = 'FedAVG')
+plt.plot(x1,y1, color = [0.1,0.53,0.93], label = 'FedAVG')
 x11,y11 = np.loadtxt('/home/cifar-gcn-drl/Test_data/test_acc_1.csv',delimiter=',',unpack = True)
-plt.plot(x11,y11, 'b--', label = 'DRL')
+plt.plot(x11,y11, color = [0.75,0.24,1], label = 'DRL')
 
 
 plt.xlabel('Time')
@@ -19,9 +19,9 @@ plt.cla()
 
 # loss
 x2, y2 = np.loadtxt('/home/cifar-gcn-drl/Test_data/FedAVG_LOSS.csv',delimiter=',',unpack = True)
-plt.plot(x2,y2, [0.75,0.24,1], label = 'FedAVG')
+plt.plot(x2,y2, color = [0.1,0.53,0.93], label = 'FedAVG')
 x21,y21 = np.loadtxt('/home/cifar-gcn-drl/Test_data/test_loss_1.csv',delimiter=',',unpack = True)
-plt.plot(x21,y21, 'b--', label = 'DRL')
+plt.plot(x21,y21, color = [0.75,0.24,1], label = 'DRL')
 
 plt.xlabel('Time')
 plt.ylabel('Loss')

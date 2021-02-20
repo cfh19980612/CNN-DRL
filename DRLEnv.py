b1f80ec5b70d8ec8155ebc3c98bd39467c91ed66
Fahao Chen <chenfh612@gmail.com>
2021-02-20 23:19:17 +0900

    .

--- DRLEnv.py
+++ DRLEnv.py
@@ -69,7 +69,7 @@ class FedEnv(gym.Env):
             trainset = torchvision.datasets.CIFAR10(
                 root='/home/ICDCS/cifar-10-batches-py/', train=True, download=True, transform=transform_train)
             trainloader = torch.utils.data.DataLoader(
-                trainset, batch_size=256, shuffle=True, num_workers=2)
+                trainset, batch_size=128, shuffle=True, num_workers=2)
 
             testset = torchvision.datasets.CIFAR10(
                 root='/home/ICDCS/cifar-10-batches-py/', train=False, download=True, transform=transform_test)
@@ -156,7 +156,7 @@ class FedEnv(gym.Env):
         # aggregate local model
         # Step 1: calculate the weight for each neighborhood
         # Step 2: aggregate the model from neighborhood
-        for i in range (2):
+        for i in range (3):
             P_new = [None for m in range (self.client)]
             for x in range (self.client):
                 P_new[x],temp = self.task.Local_agg(self.Model, x, self.client,action, self.latency)
@@ -227,12 +227,10 @@ class FedEnv(gym.Env):
 
 
         return state
-    
     def save_acc(self, X, Y, Z, i_episode):
         self.task.toCsv(X, Y, Z, i_episode)
 
     def render(self, mode='human'):
         return None
-        
     def close(self):
         return None

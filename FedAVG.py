2cefc08b0884932fc89ea99ef9920e58ba0b07bc
Fahao Chen <chenfh612@gmail.com>
2021-02-28 16:24:49 +0900

    .

--- FedAVG.py
+++ FedAVG.py
@@ -243,4 +243,4 @@ def run(dataset, net, client):
     dataframe.to_csv(location_loss,mode = 'w', header = False,index=False,sep=',')
 
 if __name__ == '__main__':
-    run(dataset = 'MNIST', net = 'MNISTNet', client = 10)
+    run(dataset = 'CIFAR10', net = 'MobileNet', client = 10)

1b7be45d6ec11e80b010eede1cac62696f732c02
Fahao Chen <chenfh612@gmail.com>
2021-03-04 16:56:31 +0900

    .

--- plot.py
+++ plot.py
@@ -42,6 +42,21 @@ fig = plt.gcf()
 fig.savefig("/home/cifar-gcn-drl/Fig/CIFAR10_Reward.eps")
 plt.cla()
 
+# topology
+x4, y4 = np.loadtxt('/home/cifar-gcn-drl/Test_data/Asyn_cifar10_ACC_base.csv',delimiter=',',unpack = True)
+plt.plot(x4,y4, linestyle = '-.', color = [0.412,0.412,0.412], label = 'Base')
+x41,y41 = np.loadtxt('/home/cifar-gcn-drl/Test_data/Asyn_cifar10_ACC_move.csv',delimiter=',',unpack = True)
+plt.plot(x41,y41, linestyle = '-', color = [1,0.65,0], label = 'Move')
+
+plt.xlabel('Time')
+plt.ylabel('Loss')
+plt.title('Comparasion')
+plt.legend()
+fig1 = plt.gcf()
+fig1.savefig("/home/cifar-gcn-drl/Fig/CIFAR10_topology.eps")
+plt.cla()
+
+
 # x4, y4 = np.loadtxt('/home/cifar-gcn-drl/Test_data/GPU.csv',delimiter=',',unpack = True)
 # plt.plot(x4,y4, color = [0.1,0.53,0.93])
 # plt.xlabel('Time (s)')
